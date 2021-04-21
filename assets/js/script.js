9bb9d9f48ba32cbe91682d278426ef0512d90f2b
Sadrac Tijerina <reavity3@gmail.com>
2021-04-21 16:31:19 -0500

    timeblocks now update according to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let currentDateObj = moment();
 $("#currentDay").text(currentDateObj.format("dddd MMMM YYYY"));
 
 let schedule = {
-  dateTime: currentDateObj,
+  dateTime: currentDateObj.format("dddd MMMM YYYY"),
   task: {
     9: "9am task",
     10: "10am task",
@@ -17,10 +17,10 @@ let schedule = {
 };
 
 //We only set it when schedule isnt created or it needs to be updated
-//localStorage.setItem("schedule", JSON.stringify(schedule));
+localStorage.setItem("schedule", JSON.stringify(schedule));
 
 function checkDateAndTime() {
-  debugger;
+  //debugger;
   let storedDate = JSON.parse(localStorage.getItem("schedule"));
 
   //There is nothing in local storage there for no schedule to get and check
@@ -28,7 +28,27 @@ function checkDateAndTime() {
     return;
   }
 
-  // ISSUE: currentDateObj and stored.dateTime return two different types.
+  if (storedDate.dateTime == currentDateObj.format("dddd MMMM YYYY")) {
+    let presentHour = parseInt(currentDateObj.format("H"));
+
+    for (i = 9; i <= 17; i++) {
+      if (i == presentHour) {
+        $("#hour-" + i).addClass("present");
+      } else if (i < presentHour) {
+        $("#hour-" + i).addClass("past");
+      } else if (i > presentHour) {
+        $("#hour-" + i).addClass("future");
+      }
+    }
+  } else {
+    updateLocalStorage();
+    //clearTasks then clear and replace local storage to current date
+  }
+}
+
+function updateLocalStorage() {
+  localStorage.clear();
+  localStorage.setItem("schedule", JSON.stringify(schedule));
 }
 /*check state of current time function
 
